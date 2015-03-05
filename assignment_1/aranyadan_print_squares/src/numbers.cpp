5511eeea0d3b37eaf53aad4ee5cd3e098407ae5a
Aranya <aranya.dan1996@gmail.com>
2015-03-06 02:38:55 +0530

    Assignment submitted by Aranya Dan 	-added the package aranyadan_print_squares
    	-added three files, numbers, squares and print in
    	aranyadan_print_squares/src which does the desired tasks

--- assignment_1/aranyadan_print_squares/src/numbers.cpp
+++ assignment_1/aranyadan_print_squares/src/numbers.cpp
@@ -5,39 +5,22 @@
 
 
 int main(int argc, char **argv)
-{
-  
-  ros::init(argc, argv, "numbers");
-
-  
-  ros::NodeHandle n;
-
-  
+{  
+  ros::init(argc, argv, "numbers");  
+  ros::NodeHandle n;  
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("topic_numbers", 1000);
-
-  ros::Rate loop_rate(1);
-
-  
+  ros::Rate loop_rate(1);  
   int count = 0;
   while (ros::ok())
-  {
-    
+  {    
     std_msgs::String msg;
-
     std::stringstream ss;
     ss << count;
-    msg.data = ss.str();
-
-
-    
+    msg.data = ss.str();    
     chatter_pub.publish(msg);
-
     ros::spinOnce();
-
     loop_rate.sleep();
     ++count;
   }
-
-
   return 0;
 }
--- assignment_1/aranyadan_print_squares/src/print.cpp
+++ assignment_1/aranyadan_print_squares/src/print.cpp
@@ -2,7 +2,6 @@
 #include "std_msgs/String.h"
 #include <string>
 #include <stdlib.h>
-
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("number=    %s", msg->data.c_str());
@@ -11,20 +10,12 @@ void chatterCallback2(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("square=    %s", msg->data.c_str());
 }
-
 int main(int argc, char **argv)
-{
-  
-  ros::init(argc, argv, "print");
-
-  
-  ros::NodeHandle n;
-  
+{  
+  ros::init(argc, argv, "print");  
+  ros::NodeHandle n;  
   ros::Subscriber sub = n.subscribe("topic_numbers", 1000, chatterCallback);
-  ros::Subscriber sub2 = n.subscribe("topic_squares", 1000, chatterCallback2);
-
-  
+  ros::Subscriber sub2 = n.subscribe("topic_squares", 1000, chatterCallback2); 
   ros::spin();
-
   return 0;
 }
